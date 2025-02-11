dacfeffc08cdbc3744e3bd22490ad32cf3aa389a
Amos Maganyane <48904837+NitionVR@users.noreply.github.com>
2025-02-11 19:15:30 +0200

    Add super problems solutions

---
+++ AlternateCase.java
@@ -0,0 +1,34 @@
+package com.enviro.assessment.grad001.amosmaganyane;
+
+public class AlternateCase {
+    public String alternateCase(String s) {
+        char[] chars = s.toCharArray();
+
+        boolean makeUpperCase = true;
+
+        for (int i = 0; i < chars.length; i++) {
+
+            if (Character.isLetter(chars[i])) {
+                chars[i] = makeUpperCase ?
+                        Character.toUpperCase(chars[i]) :
+                        Character.toLowerCase(chars[i]);
+
+                makeUpperCase = !makeUpperCase;
+            }
+        }
+
+        return new String(chars);
+    }
+
+    public static void main(String[] args) {
+        AlternateCase alternateCase = new AlternateCase();
+
+        String test1 = "hello world!";
+        String test2 = "abc123! xyz";
+        String test3 = "1 Za^B8g@E2jH*kWl!MoPqXr7YvT1c$Fs3Ud9IwZ&yX0pLkV6sHqN^tB4rA+oZ%gFj";
+
+        System.out.println(alternateCase.alternateCase(test1));
+        System.out.println(alternateCase.alternateCase(test2));
+        System.out.println(alternateCase.alternateCase(test3));
+    }
+}
---
+++ MagicalPortion.java
@@ -0,0 +1,32 @@
+package com.enviro.assessment.grad001.amosmaganyane;
+
+public class MagicalPortion {
+    public String isMagicalPotion(long power) {
+        if (power <= 0) {
+            return "NO";
+        }
+
+        long left = 1;
+        long right = 1290;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long cube = mid * mid * mid;
+
+            if (cube == power) {
+                return "YES";
+            } else if (cube < power) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return "NO";
+    }
+
+    public static void main(String[] args) {
+        MagicalPortion magicalPortion = new MagicalPortion();
+        System.out.println(magicalPortion.isMagicalPotion(132651201231L));
+    }
+}
